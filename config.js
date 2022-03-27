9daa7bd7486a07eb4037db706c59bc48aeb9f189
AryakRoy <aryakroy63@gmail.com>
2022-03-27 23:11:11 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '4776558675802892',
+        clientSecret: '97c01aa74d3aa6bbb143ad23196d2485'
+    }
 }
