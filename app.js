304008b52fed2255d57ac4917266e7ff5e994db7
AryakRoy <aryakroy63@gmail.com>
2022-04-10 17:02:50 +0530

    API Integration Setup

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var uploadRouter = require("./routes/uploadRouter");
 const favoriteRouter = require('./routes/favoriteRouter');
+var commentRouter = require('./routes/commentRouter');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url)
@@ -60,6 +61,7 @@ app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 app.use('/imageUpload', uploadRouter);
 app.use('/favorites', favoriteRouter);
+app.use('/comments', commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
