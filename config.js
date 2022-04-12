ee0f2c8021bfc9cdb6f77b0f7ab7419626803291
AryakRoy <aryakroy63@gmail.com>
2022-04-12 16:44:40 +0530

    Fixing Security Leak

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+const dotenv = require("dotenv").config()
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '4776558675802892',
-        clientSecret: '97c01aa74d3aa6bbb143ad23196d2485'
+        clientId: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
     }
 }
